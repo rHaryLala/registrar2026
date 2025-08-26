245a6aa8418b92ffaf890eae431a36ad150f3857
rHarylala <rabenamana.h@zurcher.edu.mg>
2025-08-26 09:48:51 +0300

    26 aout 2026

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -44,7 +44,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/rHaryLala',
         icon: Folder,
     },
     {
